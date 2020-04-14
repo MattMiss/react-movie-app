de6bb77b23839c5d7a034b178ebb2fe273e570df
Matthew Miss <matt.w.miss@gmail.com>
2020-04-13 23:20:49 -0700

    Program mostly works now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
+import LazyLoad from 'react-lazyload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const Spinner = () => (
+  <div className="post loading">
+    <h5>Loading...</h5>
+  </div>
+);
+
+const Post = ({id, title, body}) => (
+  <div className="post">
+
+      <div className="post-img">
+        <img src={`https://picsum.photos/id/${id}/200/200`} alt="..." />
+      </div>
+  
+    <div className="post-body">
+      <h4>{title}</h4>
+      <p>{body}</p>
     </div>
-  );
-}
+  </div>
+);
+
+const App = () => (
+  <div className="App">
+    <h2>LazyLoad Demo</h2>
+    <div className="post-container">
+      {data.map(post => (
+        <LazyLoad 
+          key={post.id} 
+          height={100}
+          offset={[-100, 100]}
+          placeholder={<Spinner />}
+        >
+          <Post key={post.id} {...post} />
+        </LazyLoad>
+      ))}
+    </div>
+  </div>
+);
+  
 
 export default App;
